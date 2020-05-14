bc6cc49578e82fd0a38d1e41c5e30f9e4a2a5739
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-14 17:08:03 +0200

    :mag: test: Make tests run.

--- test/src/dfs.js
+++ test/src/dfs.js
@@ -1,38 +1,36 @@
 import test from 'ava';
 import * as graphtraversal from '../../src';
 
-import itertools from "@aureooms/js-itertools" ;
-var list = itertools.list ;
+import { list } from "@aureooms/js-itertools" ;
 
-import collections from "@aureooms/js-collections" ;
-var set = collections.set ;
+import { set } from "@aureooms/js-collections" ;
 
-import dll from "@aureooms/js-dll" ;
+import { DoublyLinkedList } from "@aureooms/js-dll" ;
 import adjacencylist from "@aureooms/js-adjacency-list" ;
 
-var augment = require( "@aureooms/js-graph-augment" ).augment ;
+import { augment } from "@aureooms/js-graph-augment" ;
 import labeled from "@aureooms/js-graph-labeled" ;
 import sugar from "@aureooms/js-graph-sugar" ;
 
-var _MultiDiGraph = adjacencylist.MultiDiGraph( dll.DoublyLinkedList , Map ) ;
-var MultiDiGraph = augment( _MultiDiGraph ).with( labeled.label , sugar.add_missing_vertices ) ;
+const _MultiDiGraph = adjacencylist.MultiDiGraph( DoublyLinkedList , Map ) ;
+const MultiDiGraph = augment( _MultiDiGraph ).with( labeled.label , sugar.add_missing_vertices ) ;
 
-var preorder = graphtraversal.dfs_preorder ;
-var preorder_from_source = graphtraversal.dfs_preorder_from_source ;
-var postorder = graphtraversal.dfs_postorder ;
-var postorder_from_source = graphtraversal.dfs_postorder_from_source ;
+const preorder = graphtraversal.dfs_preorder ;
+const preorder_from_source = graphtraversal.dfs_preorder_from_source ;
+const postorder = graphtraversal.dfs_postorder ;
+const postorder_from_source = graphtraversal.dfs_postorder_from_source ;
 
-test( "dfs" , function ( assert ) {
+test( "dfs" , t => {
 
-	var G = new MultiDiGraph( ) ;
+	const G = new MultiDiGraph( ) ;
 
 	G.eadd( 1 , 2 ) ;
 	G.eadd( 2 , 3 ) ;
 
-	assert.t.truthy( set( preorder( G ) ).isequal( [ 1 , 2 , 3 ] ) , "preorder" ) ;
-	assert.t.deepEqual( list( preorder_from_source( G , 2 ) ) , [ 2 , 3 ] , "preorder_from_source" ) ;
-	assert.t.truthy( set( postorder( G ) ).isequal( [ 3 , 2 , 1 ] ) , "postorder" ) ;
-	assert.t.deepEqual( list( postorder_from_source( G , 2 ) ) , [ 3 , 2 ] , "postorder_from_source" ) ;
+	t.true( set( preorder( G ) ).isequal( [ 1 , 2 , 3 ] ) , "preorder" ) ;
+	t.deepEqual( list( preorder_from_source( G , 2 ) ) , [ 2 , 3 ] , "preorder_from_source" ) ;
+	t.true( set( postorder( G ) ).isequal( [ 3 , 2 , 1 ] ) , "postorder" ) ;
+	t.deepEqual( list( postorder_from_source( G , 2 ) ) , [ 3 , 2 ] , "postorder_from_source" ) ;
 
 } ) ;
 
